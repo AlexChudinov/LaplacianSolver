383d6a91dc60e2889411d1ac5271179429a5b23d
AlexChudinov <chudinov@inbox.ru>
2016-12-06 13:48:51 +0300

    Graph moved to myLib

--- LaplacianSolver/mesh_math/mesh_geometry.h
+++ LaplacianSolver/mesh_math/mesh_geometry.h
@@ -1,12 +1,11 @@
 #ifndef MESH_GEOMETRY_H
 #define MESH_GEOMETRY_H
 
-#include <vector>
-#include <utility>
 #include <map>
+#include <numeric>
 
-#include "linear_algebra/vectortemplate.h"
-#include "graph.h"
+#include <linearAlgebra\vectorTemplate.h>
+#include <data_structs\graph.h>
 
 template<typename Float, typename label> class mesh_geometry;
 
@@ -14,21 +13,19 @@ template<typename Float, typename label> class mesh_geometry;
  * Mesh connectivity and node space positions
  */
 template<typename Float, typename label>
-class mesh_geometry
+class MeshGeom
 {
 public:
-    using string         = std::string;
-    using graph          = data_structs::graph<label>;
-    using vector3f       = math::vector_c<Float, 3>;
-    using node_positions = std::vector<vector3f>;
+	using string         = std::string;
+	using node_labels    = std::vector<label>;
+	using vector3f       = math::vector_c<Float, 3>;
+	using node_positions = std::vector<vector3f>;
+	using graph          = data_structs::graph<label>;
     using box3D          = std::pair<vector3f, vector3f>;
-    using label_vector   = std::vector<label>;
-    using boundary_region_type = std::pair<graph, label_vector>; //label_vector creates boundary labels into the mesh mapping
-    using boundary_regions_list = std::map<string, boundary_region_type>;
-    using field          = std::vector<Float>;
-    using fields_list    = std::map<string, field>;
-    using field_entry    = std::pair<string, field>;
-    using boundary_entry = std::pair<string, boundary_region_type>;
+    using label_list	 = std::set<label>;
+
+    using boundaries_list= std::map<string, node_labels>;
+	using boundary_entry = std::pair<string, node_labels>;
 
     /**
      *  Node types of a mesh
@@ -40,50 +37,49 @@ public:
         BOUNDARY_FIXED_VALUE   = 0x02,
         UNKNOWN                = 0xFF ///Some error case
     };
+
     using node_types_list= std::vector<NODE_TYPE>;
 
 private:
     graph mesh_connectivity_;
     node_positions node_positions_;
-    boundary_regions_list boundary_mesh_;
-    fields_list fields_;
-    node_types_list node_types_;
+    boundaries_list boundary_mesh_;
+	node_types_list node_types_;
 
 public:
-    mesh_geometry(const graph& g, const node_positions& np)
+	MeshGeom(const graph& g, const node_positions& np)
         : mesh_connectivity_(g),
           node_positions_(np),
           node_types_(np.size(), INNER_POINT)
-    { assert(g.size() == np.size()); }
+    { 
+		if(g.size() != np.size()) throw(std::runtime_error("Graph size and node positions array size mismatch!"));
+	}
 
     /**
      * Returns all mesh connections for OpenGL drawing with GL_LINES flag
      */
-    node_positions mesh_connections() const
+    node_positions meshConnections() const
     {
-        node_positions res(mesh_connectivity_.connections_number()*2); //Each connection contains two vertices
+        node_positions res(mesh_connectivity_.connectionsNum()*2); //Each connection contains two vertices
         size_t counter = 0;
-
-        auto observer = [&res, &counter, this](size_t i, size_t j)
+        auto observer = [&](size_t i, size_t j)
         {
             res[counter++] = node_positions_[i];
             res[counter++] = node_positions_[j];
         };
-
-        mesh_connectivity_.iterate_over_unique_connections(observer);
-
+        mesh_connectivity_.iterateOverUniqueConnections(observer);
         return res;
     }
 
     /**
-     * Returns a number of nodes in the mesh
+     * Returns a number of nodes in a mesh
      */
-    inline size_t nodes_number() const { return node_positions_.size(); }
+    inline size_t size() const { return node_positions_.size(); }
 
     /**
      * Returns minimal box that contains whole mesh
      */
-    box3D containing_box() const
+    box3D box() const
     {
         Float   min_x = node_positions_[0][0],
                 min_y = node_positions_[0][1],
@@ -102,159 +98,63 @@ public:
             max_y = std::max(max_y, (*it)[1]);
             max_z = std::max(max_z, (*it)[2]);
         }
-
         return box3D({min_x, min_y, min_z}, {max_x, max_y, max_z});
     }
 
     /**
      * Checks if the boundary could be attributed to the mesh
      */
-    inline bool check_boundary(const graph& mesh) const
-    { return mesh.size() <= this->nodes_number(); }
+    inline bool checkBoundary(const boundary_entry& boundary) const
+    { return *std::max_element(boundary.second) > size(); }
     /**
-     * Checks if the mesh has a boundary with a such name
+     * Checks if the mesh already has such a boundary
      */
-    inline bool is_boundary(const string& name) const
-    { return boundary_mesh_.lower_bound(name) != boundary_mesh_.end(); }
-    /**
-     * Checks if the mesh contains field with a such name
-     */
-    inline bool is_field(const string& name) const
-    { return fields_.lower_bound(name) != boundary_mesh_.end(); }
-
+    inline bool isBoundary(const boundary_entry& boundary) const
+    { 
+		boundaries_list::const_iterator it = boundary_mesh_.lower_bound(boundary.first);
+		if (it->first != boundary.first) return false;
+		else {
+			if (boundary.second.size() != it->second.size()) return false;
+			else
+			{
+				for (size_t i = 0; i < it->second.size(); ++i)
+					if (it->second[i] != boundary.second[i]) return false;
+				return true;
+			}
+		}
+	}
     /**
      * Adds new boundary returns true if it is ok
      */
-    bool add_boundary(const string& name, const graph& mesh, NODE_TYPE type = BOUNDARY_FIXED_VALUE)
+    bool addBoundary(const boundary_entry& boundary, NODE_TYPE type = BOUNDARY_FIXED_VALUE)
     {
         //If the mesh already has this boundary
-        if(this->is_boundary(name)) return false;
+        if(isBoundary(boundary)) return false;
         //If the mesh is not fitted with the boundary
-        if(!(this->check_boundary(mesh))) return false;
+        if(!checkBoundary(boundary)) throw(std::runtime_error("Boundary contains too big labels."));
 
-        const boundary_region_type& br =
-                (this->boundary_mesh_[name] = mesh.remove_empty_connections());
-
-        auto observer = [this,type,br](label node_label)
-        {
-            this->node_types_[br.second[node_label]] = type;
-        };
-
-        br.first.dfs_iterative(observer);
+		boundary_mesh_.insert(boundary);
+		for (label l : boundary.second) node_types_[l] = type;
 
         return true;
     }
-
-    /**
-     * Sets boundary field value
-     */
-    bool set_boundary_val
-    (
-            const string& boundary_name,
-            const string& field_name,
-            const Float& val)
-    {
-        //If there is no such a boundary
-        if(!(this->is_boundary(boundary_name))) return false;
-
-        //If there is no such a field
-        if(!(this->is_field(field_name))) return false;
-
-        const field& f = this->fields_[field_name];
-        const boundary_region_type& br = this->boundary_mesh_[boundary_name];
-
-        auto observer = [val, f, br](label node_label)
-        {
-            f[br.second[node_label]] = val;
-        };
-
-        br.first.dfs_iterative(observer);
-
-        return true;
-    }
-
     /**
      * Sets boundary type
      */
-    bool set_boundary_type(const string& name, NODE_TYPE type)
+    bool setBoundaryType(const string& name, NODE_TYPE type)
     {
-        if(!(this->is_boundary(name))) return false;
-
-        const boundary_region_type& br = this->boundary_mesh_[name];
-
-        auto observer = [this, type, br](label node_label)
-        {
-            this->node_types_[br.second[node_label]] = type;
-        };
-
-        br.first.dfs_iterative(observer);
-
+        if(*boundary_mesh_.lower_bound(name) != name) return false;
+		for (label l : boundary_mesh_[name]) node_types_[l] = type;
         return true;
     }
     /**
      * Gets boundary type
      * Note: If nodes of the boundary contain different types the function returns UNKNOWN type
      */
-    NODE_TYPE get_boundary_type(const string& name) const
+    NODE_TYPE getBoundaryType(const string& name) const
     {
-        if(!(this->is_boundary(name))) return UNKNOWN;
-
-        NODE_TYPE type;
-        const boundary_region_type& br = this->boundary_mesh_.at(name);
-        type = this->node_types_[br.second[0]];
-
-        br.first.dfs_iterative([&type, this, br](label node_label)
-        {
-            if(br.second[node_label] != 0 && type != this->node_types_[br.second[node_label]])
-                type = UNKNOWN;
-        });
-
-        return type;
-    }
-    /**
-     * Adds field entry to the mesh
-     */
-    bool add_field(const string& name, const field& f)
-    {
-        //Field already exists
-        if(this->is_field(name)) return false;
-        //Field is not corresponds to the mesh
-        if(this->node_positions_.size() != f.size()) return false;
-
-       this->fields_[name] = f;
-        return true;
-    }
-
-    /**
-     * Iterates over the all boundaries
-     */
-    template<typename Observer>
-    void iterate_over_boundaries(Observer observer) const
-    {
-        for(const boundary_entry& b : this->boundary_mesh_)
-            observer(b);
-    }
-
-    /**
-     * Iterates over the all fields
-     */
-    template<typename Observer>
-    void iterate_over_fields(Observer observer) const
-    {
-        for(const field_entry& f : this->fields_)
-            observer(f);
-    }
-
-    /**
-     * Returns field refs
-     */
-    field& field_ref(const string& name)
-    {
-        return fields_.at(name);
-    }
-    const field& field_cref(const string& name) const
-    {
-        return fields_.at(name);
+		if (*boundary_mesh_.lower_bound(name) != name) return UNKNOWN;        
+		return node_types_[*boundary_mesh_[name].begin()];
     }
 };
 
--- test/test.cpp
+++ test/test.cpp
@@ -3,18 +3,23 @@
 
 #include <ls_main.h>
 #include <iostream>
-#include <mesh_math\graph.h>
+#include <mesh_math\mesh_geometry.h>
 
 int main()
 {
-	std::cout << "Dll message test: \n" << std::endl;
-	testMsg(L"Hi from dll.");
-	std::cout << "Dll message test finished!\n\n" << std::endl;
+	try 
+	{
+		std::cout << "Dll message test: \n" << std::endl;
+		testMsg(L"Hi from dll.");
+		std::cout << "Dll message test finished!\n\n" << std::endl;
 
-	std::cout << 
-
-
-
+		std::cout << "Creating mesh for cube with 1.0 side length:\n";
+		data_structs::graph<uint32_t> g;
+	}
+	catch (const std::exception& e)
+	{
+		std::cout << "Exception: " << e.what() << std::endl;
+	}
     return 0;
 }
 
